47f42b329fee2410ca0b177cead1fb402d52eda0
Lance Hsu <lancehsu12@gmail.com>
2020-07-16 21:09:22 +0800

    Add error handle

--- index.ts
+++ index.ts
@@ -1,13 +1,15 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
+import path from 'path';
+import createError from 'http-errors';
 
-import { Server } from './api/Server';
 import config from './config';
 
 const app = express();
+const { MONGODB_URL, PORT } = config;
 
-const { MONGODB_URL } = config;
 const connect = mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
 
 connect.then(
   (db) => {
@@ -16,7 +18,26 @@ connect.then(
   (err) => console.log(err)
 );
 
-const port = 8000;
+app.use(express.static(path.resolve('./') + '/build/frontend'));
 
-const server = new Server(app);
-server.start(port);
+app.use('/api', (req: Request, res: Response): void => {
+  res.send('You have reached the API!');
+});
+
+app.get('*', (req: Request, res: Response): void => {
+  res.sendFile(path.resolve('./') + '/build/frontend/index.html');
+});
+
+// catch 404 and forward to error handler
+app.use((_, __, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = err;
+  // render the error page
+  res.status(err.status ?? 500);
+  res.render('error');
+});
