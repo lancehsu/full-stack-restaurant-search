bad245b2f91adffb087a3edf0ac3602cf325faf5
Lance Hsu <lancehsu12@gmail.com>
2020-07-19 22:59:08 +0800

    Add package

---
+++ build/frontend/precache-manifest.167691d2cbb880a537d206c2d9725a20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a9dc6126bf314b4524d36eb7d3bbc9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5444b1f0b2c493398f3",
+    "url": "/static/css/main.17f79ddb.chunk.css"
+  },
+  {
+    "revision": "7feff4d0a6d0e954ebdb",
+    "url": "/static/js/2.633e9ea1.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.633e9ea1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5444b1f0b2c493398f3",
+    "url": "/static/js/main.1578a7e4.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
