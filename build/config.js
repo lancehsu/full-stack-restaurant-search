3decb083315ebbd84c844be77800b54066266d11
Lance Hsu <lancehsu12@gmail.com>
2020-07-20 00:26:41 +0800

    Deploy version

--- build/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    SECRET_KEY: '12345-67890-09876-54321',
-    MONGODB_URL: 'mongodb+srv://develop:develop@restaurant-search.xhfys.gcp.mongodb.net/restaurant_search?retryWrites=true&w=majority',
-    PORT: 3000,
-};
